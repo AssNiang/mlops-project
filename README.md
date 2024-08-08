75c9b68aff62566f09dd62d2bc67931a141a4f6c
AsNiang <nianga@ept.sn>
2024-08-08 08:46:38 +0000

    readme

--- README.md
+++ README.md
@@ -12,9 +12,6 @@ Ce projet est une application de classification de biens de consommation utilisa
 
 [Présentation MLOps : Classifiez des biens de consommation](https://www.canva.com/design/DAGNOgBf44g/YZKTTFNj6TNruYDTkm3kTA/edit?utm_content=DAGNOgBf44g&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton)
 
-## Streamlit Cloud
-
-https://mlops-project-dic-bl5xjj5ylrfw2skymfmud5.streamlit.app/
 
 ## Structure du Repository
 
--- pages/classify.py
+++ pages/classify.py
@@ -1,19 +1,7 @@
 """Streamlit app for classifying consumer goods based on description."""
 
-import requests
-import sys
-from pathlib import Path
-import pickle
 import streamlit as st
-
-sys.path.append(str(Path.cwd()))
-
-#pylint: disable=wrong-import-position
-
-from src.preprocess import text_normalizer
-from settings.params import MODEL_PARAMS
-
-#pylint: enable=wrong-import-position
+import requests
 
 # Navigation buttons
 st.page_link("streamlit_deploy.py", label="Home", icon="🏠")
@@ -41,28 +29,8 @@ try:
     )
     response.raise_for_status()  # Raise HTTPError for bad responses
     predicted_label = response.json().get("predicted_label")
-except Exception as e:
-    st.error(f"The API is unavailable due to : {e}")
-    
-    # Directly calling the model
-    # Load the classifier
-    with open("models/classifier.pkl", "rb") as pickle_in:
-        classifier = pickle.load(pickle_in)
-    # Load the vectorizer
-    with open("models/tfidf_vectorizer.pkl", "rb") as pickle_in_vect:
-        tfidf_vectorizer = pickle.load(pickle_in_vect)
-    # Normalize the input text
-    text_norm = text_normalizer(article_description)
-    # Transform the normalized text using the loaded TF-IDF vectorizer
-    text_tfidf = tfidf_vectorizer.transform([text_norm])
-    # Predict the label index using the classifier
-    predicted_label_index = classifier.predict(text_tfidf)[0]
-    # Map the label index to the actual label
-    predicted_label = (
-        ""
-        if article_description == ""
-        else MODEL_PARAMS["TARGET_LABELS"][predicted_label_index]
-    )
+except requests.RequestException as e:
+    st.error(f"An error occurred: {e}")
 
 # Display the prediction result
 st.write(f"### Predicted tag : {predicted_label if predicted_label else 'No prediction available'}")
